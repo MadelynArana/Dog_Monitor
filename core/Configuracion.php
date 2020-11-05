00b3fd4a6beb6076736d5fba9f6ba6226d88bdd9
Madelyn Arana <72529532+MadelynArana@users.noreply.github.com>
2020-11-04 18:38:20 -0600

    Update Configuracion.php

--- core/Configuracion.php
+++ core/Configuracion.php
@@ -5,12 +5,6 @@ define("BD", "monitor");
 define("CONTRASENA", "");
 define("USUARIO", "root");
 
-//define("BD", "madelein_monitor");
-//define("CONTRASENA", "#Madelyn2020");
-//define("USUARIO", "madelein_monitor");
-
-
-
 define("OPERACION", "operacion");
 define("REGISTRAR", "registro");
 define("ELIMINAR", "eliminar");
